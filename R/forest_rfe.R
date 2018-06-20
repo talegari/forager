a26089fbf0a651aa20007da2dc2a5221929b14b6
talegari <sri.teach@gmail.com>
2018-06-20 14:05:37 +0530

    finalized forest_rfe: important bugfix + enhancements for ...

--- R/forest_rfe.R
+++ R/forest_rfe.R
@@ -1,23 +1,39 @@
 #' @name forest_rfe
-#' @title lightweight implementation of RFE using ranger random forest
-#'   implementation
-#' @description Modifications to regular recursive feature elimination
-#'   procedure: \itemize{
+#' @title lightweight implementation of RFE using ranger
+#' @description For datasets with large number of predictors, this
+#'   implementation has these modifications to regular recursive feature
+#'   elimination procedure:
 #'
-#'   \item Use oob prediction error as a proxy to model performance
+#'   \itemize{
 #'
-#'   \item Build forests on samples of data and average variable importance and
-#'   oob prediction error
+#'   \item Use oob prediction error as a proxy to model performance.
+#'
+#'   \item Build forests \code{\link[ranger]{ranger}} on samples of data and
+#'   average variable importance and oob prediction error.
+#'
+#'   }
+#'
+#'   For a comprehensive RFE procedure with resampling, use
+#'   \code{\link[caret]{rfe}}
+#' @references \itemize{
+#'
+#'   \item
+#'   \href{https://topepo.github.io/caret/recursive-feature-elimination.html}{RFE
+#'    using caret}
+#'
+#'   \item
+#'   \href{http://scikit-learn.org/stable/modules/generated/sklearn.feature_selection.RFE.html}{RFE
+#'    using scikit learn}
 #'
 #'   }
 #' @param dataset (object inheriting data.frame class) A dataframe
 #' @param responseVarName (string) Name of the response variable
-#' @param sizes (integer vector) Vector of number of variables
-#' @param sampleprop (real number between 0 and 1) Proportion of observations
+#' @param sizes (integer vector) Vector of number of variables. When missing, sizes will be sequence of nc/2^i where the sequnce ranges from nc(number of columns) to 2.
+#' @param sampleprop (A real number between 0 and 1 or a vector) Proportion of observations. If not a single number and sizes is specified, this vector should have same length as sizes.
 #'   per sample
-#' @param nsamples (positive integer) Number of samples
+#' @param nsamples (positive integer or a vector) Number of samples. If not a single number and sizes is specified, this vector should have same length as sizes.
 #' @param seed (positive integer) Seed
-#' @param ... Arguments to be passed to `ranger::ranger`
+#' @param ... Arguments to be passed to \code{\link[ranger]{ranger}}
 #' @return A tibble with three columns: \itemize{
 #'
 #'   \item size: Number of variables used \item ooberror: Out-of-box error of
@@ -28,6 +44,42 @@
 #' @examples
 #' temp <- forest_rfe(iris, "Species")
 #' temp
+#'
+#' temp <- forest_rfe(iris
+#'                    , "Species"
+#'                    , sizes = c(5,4,3)
+#'                    , sampleprop = c(0.1, 0.2, 0.3)
+#'                    , nsamples = c(10, 20, 30)
+#'                    )
+#' temp
+#'
+#' temp <- forest_rfe(iris
+#'                    , "Species"
+#'                    , sizes = c(5,4,3)
+#'                    , sampleprop = 0.1
+#'                    , nsamples = c(10, 20, 30)
+#'                    )
+#' temp
+#'
+#' temp <- forest_rfe(iris
+#'                    , "Species"
+#'                    , sizes = c(5,4,3)
+#'                    , sampleprop = c(0.1, 0.2, 0.3)
+#'                    , nsamples = 10
+#'                    )
+#' temp
+#'
+#' temp <- forest_rfe(iris
+#'                    , "Species"
+#'                    , sizes = c(5, 4, 3)
+#'                    , sampleprop = c(0.1, 0.2, 0.3)
+#'                    , nsamples = 10
+#'                    , mtry = list(4, 3, 2)
+#'                    , num.trees = list(300, 500, 1000)
+#'                    , case.weights = replicate(3, runif(150), simplify = FALSE)
+#'                    )
+#' temp
+#'
 #' @export
 forest_rfe <- function(dataset
                        , responseVarName
@@ -48,55 +100,84 @@ forest_rfe <- function(dataset
   if(!missing(sizes)){
 
     assertthat::assert_that(all(sapply(sizes, assertthat::is.count)))
-    sizes <- sort(unique(sizes), decreasing = TRUE)
+    assertthat::assert_that(length(sizes) == dplyr::n_distinct(sizes))
+    sizes <- sort(sizes, decreasing = TRUE)
     assertthat::assert_that(all(sizes <= nc))
-    if(sizes[1] != nc){
-      sizes <- c(nc, sizes)
+    assertthat::assert_that(nc %in% sizes)
+    assertthat::assert_that(length(sizes) == length(sampleprop) ||
+                              length(sampleprop) == 1
+                            )
+    if(length(sampleprop) == 1){
+      sampleprop <- rep(sampleprop, length(sizes))
     }
+    assertthat::assert_that(length(sizes) == length(nsamples) ||
+                              length(nsamples) == 1
+                            )
+    if(length(nsamples) == 1){
+      nsamples <- rep(nsamples, length(sizes))
+    }
+    assertthat::assert_that(
+      all(sapply(sampleprop, function(x) dplyr::between(x, 1e-8, 1)))
+      )
+    assertthat::assert_that(
+      all(sapply(nsamples, function(x) assertthat::is.count(x)))
+      )
+
   } else {
     sizes <- unique(ceiling(sapply(0:floor(log(nc, 2)), function(x) nc/2^x)))
+    assertthat::assert_that(dplyr::between(sampleprop, 1e-8, 1))
+    assertthat::assert_that(assertthat::assert_that(assertthat::is.count(nsamples)))
+    sampleprop <- rep(sampleprop, length(sizes))
+    nsamples   <- rep(nsamples, length(sizes))
   }
 
-  assertthat::assert_that(dplyr::between(sampleprop, 1e-8, 1))
-  assertthat::assert_that(assertthat::assert_that(assertthat::is.count(nsamples)))
+  arguments      <-  list(...)
+  if(length(arguments) > 1){
+    assertthat::assert_that(
+    all(sapply(arguments, function(x) inherits(x, "list")))
+    )
+  arguments <- lapply(arguments, function(x) rep_len(x, length(sizes)))
+  }
+
   assertthat::assert_that(assertthat::assert_that(assertthat::is.count(seed)))
 
   # setup ----
-  arguments      <-  list(...)
   nr             <- nrow(dataset)
   data.table::setDT(dataset)
   predictorNames <- setdiff(colnames(dataset), responseVarName)
   if(is.null(arguments[["importance"]])){
-    arguments[["importance"]] <- "permutation"
+    arguments[["importance"]] <- as.list(rep("impurity", length(sizes)))
   }
   if(is.null(arguments[["write.forest"]])){
-    arguments[["write.forest"]] <- FALSE
+    arguments[["write.forest"]] <- as.list(rep(FALSE, length(sizes)))
   }
-  set.seed(seed)
-  seeds <- sample.int(1e6, nsamples)
 
   # given a resample index, extractImp return the vector of variable importance and oobError
-  extractImp <- function(resampleIndex){
+  extractImp <- function(resampleIndex, iter){
 
-    resampledData <- dataset[resampleIndex, ]
-    if(!is.null(arguments[["case.weights"]])){
-      resampledCaseWeights <- arguments[["case.weights"]][resampleIndex]
+    arguments_local <- lapply(arguments, function(x) `[[`(x, iter))
+    resampledData   <- dataset[resampleIndex, ]
+
+    if(!is.null(arguments_local[["case.weights"]])){
+      arguments_local[["case.weights"]] <-
+        arguments_local[["case.weights"]][resampleIndex]
+
       model <- do.call(
         ranger::ranger
-        , c(list(data = dataset
+        , c(list(data = resampledData
                  , dependent.variable.name = responseVarName
-                 , resampledCaseWeights
                  )
-            , arguments
+            , arguments_local
             )
         )
     } else {
+
       model <- do.call(
         ranger::ranger
-        , c(list(data = dataset
+        , c(list(data = resampledData
                  , dependent.variable.name = responseVarName
                  )
-            , arguments
+            , arguments_local
             )
         )
     }
@@ -111,11 +192,14 @@ forest_rfe <- function(dataset
                , value  = 1
                )
 
-  oobErrorsList       <- numeric(length = length(sizes))
+  oobErrorsList        <- numeric(length = length(sizes))
   names(oobErrorsList) <- as.character(sizes)
 
   # loop over sizes ----
   for(asizeIndex in 1:length(sizes)){
+
+    set.seed(seed)
+    seeds <- sample.int(1e6, nsamples[asizeIndex])
 
     # choose only the required columns
     removeVars <-
@@ -127,13 +211,15 @@ forest_rfe <- function(dataset
       suppressWarnings(dataset[,  c(removeVars) := NULL])
     }
 
-    imps      <- vector("list", nsamples)
-    oobErrors <- numeric(length = nsamples)
+    imps      <- vector("list", nsamples[asizeIndex])
+    oobErrors <- numeric(length = nsamples[asizeIndex])
 
     # compute importance over bootstraps
-    for(i in 1:nsamples){
+    for(i in 1:(nsamples[asizeIndex])){
       set.seed(seeds[[i]])
-      extracted <- extractImp(sample.int(nr, floor(sampleprop * nr)))
+      extracted <- extractImp(sample.int(nr, floor(sampleprop[asizeIndex] * nr))
+                              , asizeIndex
+                              )
       imps[[i]] <- extracted[[1]]
       oobErrors <- extracted[[2]]
     }
@@ -171,9 +257,11 @@ forest_rfe <- function(dataset
   }
 
   # return ----
-  result <- tibble::tibble(size       = as.integer(sizes)
-                           , ooberror = oobErrorsList
-                           , varimp   = topVarsList
+  result <- tibble::tibble(size         = as.integer(sizes)
+                           , ooberror   = oobErrorsList
+                           , varimp     = topVarsList
+                           , sampleprop = sampleprop
+                           , nsamples   = as.integer(nsamples)
                            )
 
   return(result)
